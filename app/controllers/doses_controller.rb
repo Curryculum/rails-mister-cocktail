9d1c0a95b36843d8891b39fbc6ddbec63d6c11ab
Hugo Joly <yh.joly@gmail.com>
2018-11-15 17:56:41 +0100

    add crud to cocktails and doses

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,2 +1,27 @@
 class DosesController < ApplicationController
+  def new
+    @dose = Dose.new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @ingredients = Ingredient.all
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = Cocktail.find(params[:cocktail_id])
+    if @dose.save
+      redirect_to cocktail_path(Cocktail.find(params[:cocktail_id]))
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail)
+  end
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Cocktail.destroy_all
+Ingredient.destroy_all
+
+lemon    = Ingredient.create!(name: "lemon")
+ice      = Ingredient.create!(name: "ice")
+mint     = Ingredient.create!(name: "mint leaves")
+yukon    = Ingredient.create!(name: "Yukon Jack")
+campari  = Ingredient.create!(name: "Campari")
+absinthe = Ingredient.create!(name: "Absinthe")
+whisky   = Ingredient.create!(name: "Whisky")
+jim      = Ingredient.create!(name: "Jim Beam")
+sprite   = Ingredient.create!(name: "Sprite")
+sambuca  = Ingredient.create!(name: "Sambuca")
+pisco    = Ingredient.create!(name: "Pisco")
+
+
+mojito  = Cocktail.create!(name: "Mojito")
+caipi   = Cocktail.create!(name: "Caipi")
+
+p "add doses in mojito..."
+Dose.create!(ingredient: mint, cocktail: mojito, description: "3 feuilles")
+Dose.create!(ingredient: ice, cocktail: mojito, description: "2 cubes")
+Dose.create!(ingredient: lemon, cocktail: mojito, description: "1 zeste")
+
+p "add doses in caipi..."
+Dose.create!(ingredient: sprite, cocktail: caipi, description: "3 toto")
+Dose.create!(ingredient: pisco, cocktail: caipi, description: "2 tata")
